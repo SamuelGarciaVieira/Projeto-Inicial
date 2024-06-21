ee601885b184317260b1a722130436aa9b5bc428
Samuel Garcia <samuelportovieira@gmail.com>
2024-06-21 16:03:48 -0300

    Repositorios Configurados e começo da pagina Repo

--- samuel_vieira_1522645/public/assets/scripts/script.js
+++ samuel_vieira_1522645/public/assets/scripts/script.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function () {
     let data = {};
     fetch("https://api.github.com/users/SamuelGarciaVieira")
         .then(res => res.json())
@@ -12,8 +12,9 @@ document.addEventListener("DOMContentLoaded", function(){
             let resp = json;
             MostrarRepo(resp);
         })
+        .catch(error => console.error(console.error("Erro ao buscar dados JSON")));
 
-    function MostrarPerfil(data){
+    function MostrarPerfil(data) {
         const mostrartela = document.getElementById("mostrarperfil");
         const div = document.createElement("div");
         div.className = 'row';
@@ -22,11 +23,11 @@ document.addEventListener("DOMContentLoaded", function(){
                     <img class="border border-3 rounded-start img-fluid d-block" src="${data.avatar_url}" alt="perfil">
                 </div>
                 <div class="col-md-9 align-items-start">
-                    <p class="text-primary fw-semibold mt-2 fs fs-4">Samuel Garcia</p>
+                    <p class="text-primary fw-semibold mt-2 fs fs-4">${data.name}</p>
                     <p>
-                        Meu nome é Samuel Garcia e sou estudante de Sistemas de Informação na PUC Minas. Tenho uma grande paixão por tecnologia e inovação, e estou sempre buscando novas                       formas de aplicar meus conhecimentos para resolver problemas reais. Comprometido com a aprendizagem contínua, tenho interesse em áreas como desenvolvimento de                      software, análise de dados e segurança da informação. Estou determinado a me tornar um profissional de destaque na área de TI.
+                        ${data.bio}    
                     </p>
-                    <p><span class="fw-bold">Localização: </span>Belo Horizonte, MG, Brazil</p>
+                    <p><span class="fw-bold">Localização: </span>${data.location}</p>
                     <p>
                        <span class="p-1">
                             <a target="_blank" class="ml text-dark text-decoration-none" href="https://www.linkedin.com/in/samuelgarciavieira/"> 
@@ -49,33 +50,41 @@ document.addEventListener("DOMContentLoaded", function(){
         mostrartela.appendChild(div);
     }
 
-    function MostrarRepo(resp){
+    function MostrarRepo(resp) {
         const mostrarepo = document.getElementById("mostrarRepo");
         const div = document.createElement("div");
         div.className = 'row';
         resp.forEach(element => {
             const div2 = document.createElement("div");
-            div2.className = 'col-lg-3';
+            div2.className = 'col-lg-2 mt-3';
+            div2.id = `repo-${element.id}`;
             div2.innerHTML = `
-                <div class="card">
-                    <div class="card-header">Projeto Primeiro Site com Bootstrap </div>
+                <div class="card ">
+                    <div class="card-header">${element.name}</div>
                     <div class="card-body">
-                        <p>Projeto referente a este site, primeiro site completo e responsivo, criado inicialmente como trabalho de <span class="fw-semibold">DIW</span>.</p>
-                        <button type="button"class="btn btn-light">
-                            <a target="_blank" class="text-dark text-decoration-none" href="https://github.com/SamuelGarciaVieira/Projeto-Primeiro-Site?tab=readme-ov-file#projeto-inicial">
-                                <i class="bi bi-github h4" id="icones"></i>
-                                <span class="fw-bold">Repositorio</span>
-                            </a>
-                        </button>
-                        <button type="button"class="btn btn-light">
-                           <a class="text-dark text-decoration-none" href="repo.html">
-                               <span class="fw-semibold">Saiba Mais</span>
-                           </a>
-                        </button>
-                    </div>
+                        <p>${element.description}</p>
+                    <button type="button"class="btn btn-light">
+                        <a target="_blank" class="text-dark text-decoration-none stretched-link" href="repo.html">
+                        <span class="fw-bold" id="informacao">Saiba Mais</span>
+                    </a>
+                    </button>
+                    <span class="p-2">
+                        <i class="bi bi-person-fill h4 "></i> ${element.watchers}
+                    </span>
+                    <i class="bi bi-star h4"></i> ${element.watchers}
+                </div>
             `;
             div.appendChild(div2);
         });
         mostrarepo.append(div);
     }
+
+    document.querySelectorAll('#informacao').forEach(button => {
+        button.addEventListener('click', function(event) {
+            const repoId = this.getAttribute('data-id');
+            localStorage.setItem('selectedRepoId', repoId);
+            window.location.href = 'repo.html';  // Redireciona para a nova página
+        });
+    });
+    
 })
